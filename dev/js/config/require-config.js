1c1ade170507d52382c09bec5260312ee5afd0d0
Shouhiro <1406635792@qq.com>
2016-09-07 19:14:50 +0800

    validate
    validate form by email & password

---
+++ dev/js/config/require-config.js
@@ -0,0 +1,23 @@
+/**
+ * Created by HIRO on 2016/9/7.
+ */
+requirejs.config({
+    baseUrl: "/easyjob/pro/js",
+    shim: {
+        "semantic":{
+            deps: ["jquery"]
+        },
+        "login": {
+            deps: ["jquery","semantic","react","reactDOM"]
+        }
+    },
+    paths: {
+        "login":"view/login.view",
+
+        "jquery":"vender/jquery/jquery-3.1.0.min",
+        "semantic":"vender/semantic/semantic.min",
+        "react":"vender/react/react.min",
+        "reactDOM":"vender/react/react-dom.min"
+    },
+    urlArgs:"v=201609071150"
+});
---
+++ dev/js/view/login.view.js
@@ -0,0 +1,24 @@
+define(function(){
+	$('#loginForm').form({
+		on:"blur",
+		fields:{
+			email:{
+				identifier:'email',
+				rules:[{
+					type:'empty',
+					prompt:'请输入邮箱账号'
+				},{
+					type:'email',
+					prompt:'请输入正确的邮箱账号'
+				}]
+			},
+			password:{
+				identifier:'password',
+				rules:[{
+					type:'empty',
+					prompt:'请输入密码'
+				}]
+			}
+		}
+	});
+});
