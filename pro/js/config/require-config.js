8347f4ef6045065ec2ed607ff1f243fb16dad56c
1002271511 <18613853895@163.com>
2016-09-15 11:21:51 +0800

    change

--- pro/js/config/require-config.js
+++ pro/js/config/require-config.js
@@ -1,29 +1,3 @@
-"use strict";
-
-/**
+"use strict";/**
  * Created by HIRO on 2016/9/7.
- */
-requirejs.config({
-    baseUrl: "/easyjob/pro/js",
-    shim: {
-        "semantic": {
-            deps: ["jquery"]
-        },
-        "login": {
-            deps: ["jquery", "semantic", "react", "reactDOM"]
-        },
-        "gittest": {
-            deps: ["jquery", "semantic"]
-        }
-    },
-    paths: {
-        "login": "view/login.view",
-        "gittest": "view/gittest.view",
-
-        "jquery": "vender/jquery/jquery-3.1.0.min",
-        "semantic": "vender/semantic/semantic.min",
-        "react": "vender/react/react.min",
-        "reactDOM": "vender/react/react-dom.min"
-    },
-    urlArgs: "v=201609071150"
-});
+ */requirejs.config({baseUrl:"/easyjob/pro/js",shim:{"semantic":{deps:["jquery"]},"login":{deps:["jquery","semantic","react","reactDOM"]},"gittest":{deps:["jquery","semantic"]}},paths:{"login":"view/login.view","gittest":"view/gittest.view","jquery":"vender/jquery/jquery-3.1.0.min","semantic":"vender/semantic/semantic.min","react":"vender/react/react.min","reactDOM":"vender/react/react-dom.min"},urlArgs:"v=201609071150"});
